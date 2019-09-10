018ee02e43a5c6d96a9064a73dd1b222f11f75cd
benjleis978 <benjleis978@SNS-D-130RZW>
2019-09-10 13:19:39 -0400

    Fixed some formatting

--- IKEA/Form1.cs
+++ IKEA/Form1.cs
@@ -27,7 +27,7 @@ namespace IKEA
             //Graphic objects
             Graphics g = this.CreateGraphics();
             Pen drawPenB = new Pen(Color.Blue, 5);
-            Pen drawPenC = new Pen(Color.Blue, 1);
+            Pen drawPenR = new Pen(Color.Blue, 1);
             Pen drawPenY = new Pen(Color.Yellow, 5);
             SolidBrush drawBrushY = new SolidBrush(Color.Yellow);
             SolidBrush drawBrushB = new SolidBrush(Color.Blue);
@@ -39,8 +39,7 @@ namespace IKEA
             g.FillRectangle(drawBrushB, 40, 40, 200, 100);
             g.DrawEllipse(drawPenY, 45, 55, 190, 70);
             g.FillEllipse(drawBrushY, 45, 55, 190, 70);
-            //Ristricted symbol
-            g.DrawEllipse(drawPenC, 198, 67, 10, 10);
+            g.DrawEllipse(drawPenR, 198, 67, 10, 10);
 
             //Code for Text
             g.DrawString("IKEA", drawFont, drawBrushB, 70, 60);
